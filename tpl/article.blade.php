6ca494019343afe104fd0b313d9fcb5c0d2308ba
Aruna <myruala@gmail.com>
2019-02-12 18:07:06 +0530

    styles upd

---
+++ tpl/article.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+@section('content')
+    <div class="section content-page">
+        <div class="container">
+            @component('components::admin', ['links' => $article->admin()]) @endcomponent
+            <h1 class="mb-2 mb-sm-4">{!! $article->name !!}</h1>
+            <div class="faq-article">{!! $article->text !!}</div>
+            <div class="faq-controls">
+                @if(($prev = $article->prev()->first()))
+                    <a href="/{{$article->section->slug}}/{{$prev->slug}}" class="btn btn-secondary mr-2" title="{{ __('article.prev') }}"><i class="fas fa-chevron-left"></i></a>
+                @endif
+                @if(($next = $article->next()->first()))
+                    <a href="/{{$article->section->slug}}/{{$next->slug}}" class="btn tm-btn-red" title="{{ __('article.next') }}"><i class="fas fa-chevron-right"></i></a>
+                @endif
+            </div>
+            <div>
+                <a href="/{{$article->section->slug}}" class="tm-btn-back btn btn-secondary" title="{{ __('article.back') }}"><i class="fas fa-chevron-left"></i> {{ __('article.back') }}</a>
+            </div>
+        </div></div>
+@endsection
+@section('meta_title'){{$article->meta_title}}@endsection
+@section('meta_keywords'){{$article->meta_keywords}}@endsection
+@section('meta_description'){{$article->meta_description}}@endsection
---
+++ tpl/section.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+@section('content')
+    <div class="section content-page">
+        <div class="container">
+            @component('components::admin', ['links' => $section->admin()]) @endcomponent
+            <h1 class="mb-2 mb-sm-4">{!! $section->name !!}</h1>
+            @if($articles)
+                <div id="faq-accordion" class="accordion mb-5">
+                @foreach($articles as $k=>$article)
+                    <div class="card">
+                        @component('components::admin', ['links' => $article->admin()]) @endcomponent
+                        <div class="card-header @if($k != 0) collapsed @endif" data-toggle="collapse" data-target="#faqCollapse-{{$k}}">
+                            <h3 class="mb-0">{!! $article->name !!}</h3>
+                        </div>
+                        <div id="faqCollapse-{{$k}}" class="collapse @if($k == 0) show @endif" data-parent="#faq-accordion">
+                            <div class="card-body">
+                                <p>{!! $article->description !!}</p>
+                                <div class="text-right">
+                                    <a href="/{{$section->slug}}/{{$article->slug}}" class="btn tm-btn-red">{{ __('article.read_more') }}</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+                    <div class="pagination-wrapper"> {{$articles->links()}} </div>
+                </div>
+                @endif
+        </div></div>
+@endsection
+@section('meta_title'){{$section->meta_title}}@endsection
+@section('meta_keywords'){{$section->meta_keywords}}@endsection
+@section('meta_description'){{$section->meta_description}}@endsection
