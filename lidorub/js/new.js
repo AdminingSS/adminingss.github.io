cc52499d73872e596f5a00b4fc491ab90c20de71
AdminingSS <admining@mail.ru>
2020-02-27 08:14:04 +0200

    site 7.10

--- lidorub/js/new.js
+++ lidorub/js/new.js
@@ -16,4 +16,34 @@ $(document).ready(function () {
 
     })();
 
+    (()=>{
+        const $orderModal = $('.js-order-modal');
+
+        const $jsSubmitStageOne = $('.js-submit-stage-1');
+
+        const $jsSubmitStageThree = $('.js-submit-stage-3');
+        const $jsSubmitStageFour = $('.js-submit-stage-4');
+        const $jsSubmitStageFive = $('.js-submit-stage-5');
+        const $jsSubmitStageSix = $('.js-submit-stage-6');
+
+        $jsSubmitStageOne.on('click', function (e) {
+            if(!$(this).hasClass('active')) return;
+
+            $orderModal.find('.step2 .inactive').fadeOut();
+            $orderModal.find('.step2 .offers').slideDown(500).fadeIn({duration: 500, queue: false});
+
+            const $jsSubmitStageTwo = $('.js-submit-stage-2');
+
+            $jsSubmitStageTwo.on('click', function () {
+                console.log('done');
+
+                $orderModal.find('.step3 .inactive').fadeOut();
+                $orderModal.find('.step3 .options').slideDown(500).fadeIn({duration: 500, queue: false});
+            });
+        });
+
+
+
+    })();
+
 });
