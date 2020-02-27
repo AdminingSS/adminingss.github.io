b3120760b3eb712b2c7b9682315f34a7303d7c65
AdminingSS <admining@mail.ru>
2020-02-27 18:37:22 +0200

    site 7.11

--- lidorub/js/new.js
+++ lidorub/js/new.js
@@ -4,20 +4,12 @@ $(document).ready(function () {
     (()=>{
         const $orderModal = $('.js-order-modal');
         const $orderModalTrigger = $('.js-order-modal-trigger');
-        const $orderModalClode = $('.js-order-modal-close');
+        const $orderModalClose = $('.js-order-modal-close');
 
-        $orderModalTrigger.on('click', function () {
-            $orderModal.show();
-        });
+        const $orderModal2 = $('.js-order-modal-2');
+        const $orderModalClose2 = $('.js-order-modal-close-2');
 
-        $orderModalClode.on('click', function () {
-            $orderModal.hide();
-        });
-
-    })();
-
-    (()=>{
-        const $orderModal = $('.js-order-modal');
+        const $orderModalReturn2 = $('.js-order-modal-return-2');
 
         const $jsSubmitStageOne = $('.js-submit-stage-1');
 
@@ -25,6 +17,23 @@ $(document).ready(function () {
         const $jsSubmitStageFour = $('.js-submit-stage-4');
         const $jsSubmitStageFive = $('.js-submit-stage-5');
         const $jsSubmitStageSix = $('.js-submit-stage-6');
+
+        $orderModalTrigger.on('click', function () {
+            $orderModal.show();
+        });
+
+        $orderModalClose.on('click', function () {
+            $orderModal.hide();
+        });
+
+        $orderModalClose2.on('click', function () {
+            $orderModal2.hide();
+        });
+
+        $orderModalReturn2.on('click', function () {
+            $orderModal2.hide();
+            $orderModal.show();
+        });
 
         $jsSubmitStageOne.on('click', function (e) {
             if(!$(this).hasClass('active')) return;
@@ -35,11 +44,18 @@ $(document).ready(function () {
             const $jsSubmitStageTwo = $('.js-submit-stage-2');
 
             $jsSubmitStageTwo.on('click', function () {
-                console.log('done');
-
                 $orderModal.find('.step3 .inactive').fadeOut();
                 $orderModal.find('.step3 .options').slideDown(500).fadeIn({duration: 500, queue: false});
+
+                const $orderModalTrigger2 = $('.js-order-modal-trigger-2');
+
+                $orderModalTrigger2.on('click', function () {
+                    $orderModal.hide();
+                    $orderModal2.show();
+                });
             });
+
+
         });
 
 
